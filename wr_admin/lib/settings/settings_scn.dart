b6c459aeb20599d153d3192841ca5bb0e13c4d7f
varun pc <varunrevati6@gmail.com>
2026-02-25 23:37:46 +0530

    Settings UI Done

---
+++ wr_admin/lib/settings/settings_scn.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:wr_admin/mobile_layout.dart';
+import 'package:wr_admin/settings/dashboard_scn.dart';
+import 'package:wr_admin/tablet_layout.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth < 600) {
+          return const MobileLayout();
+        } else if (constraints.maxWidth < 1024) {
+          return const TabletLayout();
+        } else {
+          return const DesktopLayout();
+        }
+      },
+    );
+  }
+}
